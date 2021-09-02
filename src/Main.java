36edd0079f5c172ef0f8a52051ba86704e24f7cf
Filip <szymanski.fp@gmail.com>
2021-09-02 17:16:32 +0200

    working response on connection

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var s = new ServerSocket(8189);
+        Socket incoming = s.accept();
+
+        InputStream inStream = incoming.getInputStream();
+        OutputStream outStream = incoming.getOutputStream();
+
+        var in = new Scanner(inStream, StandardCharsets.UTF_8);
+        var out = new PrintWriter(new OutputStreamWriter(outStream, StandardCharsets.UTF_8), true);
+
+        out.println("Hello, type bye to exit!");
+        String line = in.nextLine();
+        out.println("Echo: " + line);
+
+        boolean done;
+        if(line.trim().equals("bue")) {
+            done = true;
+            incoming.close();
+        }
+
+
+    }
+}
